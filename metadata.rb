d7d07c560f82ca6446530642a6b401e68771e698
Tim Smith <tsmith84@gmail.com>
2015-09-07 21:58:02 -0700

    Updates to the metadata
    Add long_description
    Add OS supports
    Add supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,15 @@
 name 'yum-chef'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'apache 2.0'
+license 'Apache 2.0'
 description 'Configures yum repository for Chef Software, Inc. products'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
-depends 'yum'
+depends 'yum', '~> 3.2'
+
+%w(amazon centos fedora oracle redhat scientific).each do |os|
+  supports os
+end
+
+source_url 'https://github.com/chef-cookbooks/yum-chef' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/yum-chef/issues' if respond_to?(:issues_url)
