97081b374fb50525fb1b4a73de1001e7c9d7063b
Tim Smith <tsmith@chef.io>
2016-09-06 14:47:30 -0700

    Allow running the yum 4.0 cookbook
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'Apache 2.0'
 description 'Configures yum repository for Chef Software, Inc. products'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
-depends 'yum', '>= 3.2'
+depends 'yum', '>= 3.2', '< 5.0'
 
 %w(amazon centos fedora oracle redhat scientific).each do |os|
   supports os
