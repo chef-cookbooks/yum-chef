5eb35eb3efe11a04f772f1444bf36b61ef0c4c98
Tim Smith <tsmith@chef.io>
2016-11-25 14:59:16 -0800

    Update copyright headers

--- recipes/current.rb
+++ recipes/current.rb
@@ -1,9 +1,9 @@
 #
-# Cookbook Name:: yum-chef
+# Cookbook:: yum-chef
 # Recipe:: current
 #
 # Author:: Joshua Timberman <joshua@chef.io>
-# Copyright (c) 2015-2016, Chef Software, Inc. <legal@chef.io>
+# Copyright:: 2015-2016, Chef Software, Inc. <legal@chef.io>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
