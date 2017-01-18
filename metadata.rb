1e86499fa8f4697652525ab918e49591e8b0fd88
Tim Smith <tsmith@chef.io>
2017-01-17 21:09:43 -0800

    Testing updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license 'Apache 2.0'
 description 'Configures yum repository for Chef Software, Inc. products'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '3.0.1'
+
 depends 'compat_resource', '>= 12.16.3'
 
 %w(amazon centos fedora oracle redhat scientific).each do |os|
@@ -13,5 +14,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/yum-chef'
 issues_url 'https://github.com/chef-cookbooks/yum-chef/issues'
-
-chef_version '>= 12.1'
+chef_version '>= 12.1' if respond_to?(:chef_version)
