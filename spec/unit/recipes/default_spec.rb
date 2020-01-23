992c5511213571cbc50b0424dc72df9c33f57e2c
Tim Smith <tsmith84@gmail.com>
2020-01-22 21:14:28 -0800

    Avoid deprecation warnings in the ChefSpec tests
    Use the major version instead of a minor
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -34,7 +34,7 @@ describe 'yum-chef::default' do
     let(:chef_run) do
       ChefSpec::SoloRunner.new(
         platform: 'centos',
-        version: '7.3.1611'
+        version: '7'
       ).converge(described_recipe)
     end
 
